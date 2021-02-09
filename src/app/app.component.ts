f3cf476d93397810a74010d24e395d79ec7704d2
Jesús Ángel Martínez Bello <arcelormittaldeveloper@gmail.com>
2021-02-09 11:31:11 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl : 'app.component.html'
 })
-export class AppComponent {
-  title = 'bases';
+
+export class AppComponent{
+
 }
+
+/*
+title: string = 'Contador App';
+  numero: number = 10;
+
+  acumular(valor: number){
+    this.numero += valor;
+  }
+
+  base: number = 5;
+
+  sumar(){
+    this.numero += 1;
+  }
+
+  restar(){
+    this.numero -= 1;
+  }
+  */
